1ba325899ff17eac1576ebc9c7b03bbecbc3499f
lespinozae <lmanuel2007@yahoo.es>
2016-05-26 14:13:02 -0600

    segunda entrega

---
+++ core/nuevo/zona_privada.php
@@ -0,0 +1,50 @@
+
+<?php
+require_once('DBConnector.php');
+require_once('mStatic.php');
+/* Inicia nueva sesion */ 
+session_start();
+
+
+if($_SESSION["user"]==""){ 
+   
+	header("location:index.php");
+	exit(); 
+	} 
+/* Tiempo de caducidad de la sesión actual */
+        $inactivo = 1800;
+        $vida_session = 0;
+        
+    if (isset($_SESSION["time"]))
+    {
+        $vida_session = time() - $_SESSION["time"];
+        $valor = false;
+        
+        $self = $_SERVER['PHP_SELF'];
+        $valor = strpos('user', $self);
+        $valores = mStatic::url_acces($_SESSION["user"]);
+       
+        for($i = 0; $i<count($valores); $i++)
+        {
+            if(strlen($valores[$i]["archivo"]) > 0)
+            {
+                $valor = strpos($self, $valores[$i]["archivo"]);
+            }
+            if($valor) break;
+        }
+        if($valor == false and !strpos($self, '/cajax/'))
+        {
+            echo '<script>window.location.href="index.php";</script>';
+        }
+        
+        if ($vida_session > $inactivo)
+        {
+            echo '<script>alert("Su sesión ha caducado")</script>'; //Script de caducidad de la sesión
+            echo '<script>window.location.href="index.php";</script>';
+            $_SESSION = array();
+            session_destroy();
+        }
+    }
+    
+    $_SESSION["time"] = time();
+?>
